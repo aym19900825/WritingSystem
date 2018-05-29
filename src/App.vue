fe65342b362f5165f12553e6ed669e594d2f8d06
aym19900825 <825185228@qq.com>
2018-05-29 18:25:18 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header class="header" height="100px">
+        <h3>writing system</h3>
+        <p>login module</p>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -12,12 +19,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .header{
+    background-color: #409EFF;
+    color: white;
+  }
+  .header h3,p{
+    text-align:center;
+  }
 </style>
+    
---
+++ src/page/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
